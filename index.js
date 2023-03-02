02936adab414c41fe8e421ecee05124ea992afe0
luckyian <ian.wren81@gmail.com>
2023-03-01 21:04:39 -0800

    Added code to test express routing

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
-import express from "express";
+// import express from "express";
+
+app.get('/', (req,res)=>{
+    res.send({hi: 'there'})
+
+
+});
+
+app.listen(3000);
